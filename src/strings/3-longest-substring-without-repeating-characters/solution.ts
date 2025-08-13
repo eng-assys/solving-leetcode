611dbecee0ea55b9d74e40ca5999d2b26132d3a5
Lucas Assis <lukas.ecomp@gmail.com>
2025-08-13 09:55:59 -0300

    first attempt of lengthOfLongestSubstring

--- src/strings/3-longest-substring-without-repeating-characters/solution.ts
+++ src/strings/3-longest-substring-without-repeating-characters/solution.ts
@@ -1,3 +1,21 @@
-function lengthOfLongestSubstring(s: string): number {
+export function lengthOfLongestSubstring(s: string): number {
+    if(s.length === 0) return 0;
+
+    let substring = [s[0]];
+    let maxLength = 1;
+    let i = 1;
+
+    while(i < s.length){
+        const charInSubstring = substring.indexOf(s[i]);
+        if(charInSubstring === -1)
+            substring.push(s[i]);
+        else
+            substring = substring.slice(charInSubstring);
+        
+        maxLength = Math.max(maxLength, substring.length);
+
+        i += 1;
+    }
     
+    return maxLength;
 };
