5c3cffe72651b0b69621aeb3508b13c66d266be3
Lucas Assis <lukas.ecomp@gmail.com>
2025-08-14 09:48:04 -0300

    add better variable description

--- src/strings/409-longest-palindrome/solution.ts
+++ src/strings/409-longest-palindrome/solution.ts
@@ -7,17 +7,17 @@ export function longestPalindrome(s: string): number {
         else charMap[s[i]] = 1;
     }
 
-    let addPlusOne = false;
+    let odd = false;
     for (const key in charMap){
         if(charMap[key] % 2 === 0) longestPalindromeLength += charMap[key];
-        else if(charMap[key] === 1) addPlusOne = true;
+        else if(charMap[key] === 1) odd = true;
         else {
             longestPalindromeLength += charMap[key] - 1
-            addPlusOne = true;
+            odd = true;
         };
     }
 
-    if(addPlusOne) longestPalindromeLength += 1;
+    if(odd) longestPalindromeLength += 1;
 
     return longestPalindromeLength;
 };
